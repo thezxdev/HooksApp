9c68282353075d094a271669ac779ee50ce1ec47
Thezx <thezxdev@gmail.com>
2022-08-09 02:07:21 -0700

    creacion del custom hook useCounter

---
+++ src/01-useState/CounterWithCustomHook.jsx
@@ -0,0 +1,27 @@
+import { useCounter } from '../hooks/useCounter';
+
+export const CounterWithCustomHook = () => {
+
+  const { counter } = useCounter();
+
+  return (
+    <>
+      <h1>CounterWithCustomHook: { counter }</h1>
+
+      <hr />
+
+      <button
+        className="btn btn-primary"
+      >+1</button>
+
+      <button
+        className="btn btn-primary"
+      >Reset</button>
+
+      <button
+        className="btn btn-primary"
+      >-1</button>
+
+    </>
+  )
+}
