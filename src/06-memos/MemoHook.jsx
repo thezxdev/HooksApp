cd48c0ab734704ac9439cc0da8e85b619b2561c9
Thezx <thezxdev@gmail.com>
2022-08-10 00:11:33 -0700

    uso del hook useMemo

---
+++ src/06-memos/MemoHook.jsx
@@ -0,0 +1,45 @@
+import { useState, useMemo } from 'react';
+import { useCounter } from '../hooks';
+
+const heavyStuff = ( iterationNumber = 100 ) => {
+
+  for (let i = 0; i < iterationNumber; i++) {
+    console.log('Ahí vamos');
+  }
+
+  return `${ iterationNumber } Iteraciones realizadas`;
+}
+
+export const MemoHook = () => {
+
+  const { counter, increment } = useCounter( 4000 );
+  const [ show, setShow ] = useState( true );
+
+  const memorizedValue = useMemo(
+    () => heavyStuff( counter ), // Valor a memorizar | la función en este caso
+    [ counter ] // Dependencias que necesarias para reprocesar el antiguo valor
+  );
+
+  return (
+    <>
+      <h1>Counter: <small>{ counter }</small> </h1>
+      <hr />
+
+      <h4>{ memorizedValue }</h4>
+
+      <button
+        className="btn btn-primary"
+        onClick={ () => increment() }
+      >
+        +1
+      </button>
+
+      <button
+        className="btn btn-outline-primary"
+        onClick={ () => setShow( !show ) }
+      >
+        Show/Hide { JSON.stringify( show ) }
+      </button>
+    </>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Memorize } from './06-memos/Memorize';
+import { MemoHook } from './06-memos/MemoHook';
+// import { Memorize } from './06-memos/Memorize';
 // import { Layout } from './05-useLayoutEffect/Layout';
 // import { FocusScreen } from './04-useRef/FocusScreen';
 // import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
@@ -13,6 +14,6 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Memorize />
+    <MemoHook />
   </React.StrictMode>
 )
