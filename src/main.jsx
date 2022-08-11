1223fb9f0083a9758078d4d3c980dc8daea6682f
Thezx <thezxdev@gmail.com>
2022-08-11 12:29:42 -0700

    Implementacion del useReducer

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Padre } from './07-tarea-memo/Padre';
+// import { Padre } from './07-tarea-memo/Padre';
 // import { CallbackHook } from './06-memos/CallbackHook';
 // import { MemoHook } from './06-memos/MemoHook';
 // import { Memorize } from './06-memos/Memorize';
@@ -12,12 +12,13 @@ import { Padre } from './07-tarea-memo/Padre';
 // import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 // import { CounterApp } from './01-useState/CounterApp';
 // import { HooksApp } from './HooksApp';
+import { TodoApp } from './08-useReducer/TodoApp';
 import './index.css';
 
-import './08-useReducer/intro-reducer';
+// import './08-useReducer/intro-reducer';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Padre />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <TodoApp />
+  // </React.StrictMode>
 )
