41771b53921863e7c1782cfa0d351acae8e53a16
Thezx <thezxdev@gmail.com>
2022-08-12 20:08:57 -0700

    Obtener valores del context y cambiar valores del context

--- src/09-useContext/LoginPage.jsx
+++ src/09-useContext/LoginPage.jsx
@@ -1,7 +1,25 @@
+import { useContext } from 'react';
+import { UserContext } from './context/UserContext';
+
 export const LoginPage = () => {
+
+  // Utilización del useContext, recibe como parámetro el contexto que queremos obtener.
+  const { user, setUser } = useContext( UserContext );
+  console.log( user );
+
   return (
     <>
       <h1>LoginPage</h1>
+
+      <pre>
+        { JSON.stringify( user, null, 3 ) }
+      </pre>
+
+      <button
+        onClick={ () => setUser( { id: 123, name: 'zxzx', email: 'zxzx@gmail.com' } ) }
+        className="btn btn-primary">
+        Set User
+      </button>
     </>
   )
 }
--- src/09-useContext/context/UserProvider.jsx
+++ src/09-useContext/context/UserProvider.jsx
@@ -1,15 +1,20 @@
+import { useState } from 'react';
 import { UserContext } from './UserContext';
 
-const user = {
-  id: 123,
-  name: 'zxzx',
-  email: 'zxzx@gmail.com'
-}
+// const user = {
+//   id: 123,
+//   name: 'zxzx',
+//   email: 'zxzx@gmail.com'
+// }
 
 // Creación del Provider
 export const UserProvider = ({ children }) => {
+  
+  const [user, setUser] = useState();
+
   return (
-    <UserContext.Provider value={{ hola: 'Mundo', user }}>
+    // <UserContext.Provider value={{ hola: 'Mundo', user }}>
+    <UserContext.Provider value={{ user, setUser }}>
       { children }
     </UserContext.Provider>
   )
