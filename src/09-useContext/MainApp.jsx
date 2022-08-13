bf9a18da5c9f43a8561f919e73def0f467a1014c
Thezx <thezxdev@gmail.com>
2022-08-12 17:58:43 -0700

    Definir rutas de la aplicacion

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,9 +1,26 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage';
+
+// Componente en el que se van a mostrar todas las rutas
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
       <hr />
-      
+
+      {/* Rutas de la aplicación */}
+      <Routes>
+        <Route path="/" element={ <HomePage /> } />
+        <Route path="login" element={ <LoginPage /> } />
+        <Route path="about" element={ <AboutPage /> } />
+        {/* Cargar un componente cuando no se encuentre una ruta */}
+        {/* <Route path="/*" element={ <LoginPage /> } /> */}
+        {/* Mover a una ruta cuando no se encuentre una ruta */}
+        <Route path="/*" element={ <Navigate to="/about" />  } />
+      </Routes>
+
     </>
   )
 }
